3ec5f68dc19a3109eb0e1c2da1cbfd53c96d4a16
PROX-E <lichlord18@gmail.com>
2020-05-29 22:57:12 -0600

    add generate html function to create page for resume

--- Develop/index.js
+++ Develop/index.js
@@ -116,3 +116,18 @@ const colors = [
        photoBorderColor: "white"
      }
  ];
+
+ function generateHTML(data) { // Generates HTML based on data given to create a PDF resume
+    return `<!DOCTYPE html>
+  <html lang="en">
+     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+        <title>Resume</title>
+        
+  </body>
+        `
+          };
