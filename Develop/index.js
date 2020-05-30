c82eee19f7693543460d73a1be14c3e29f86b366
PROX-E <lichlord18@gmail.com>
2020-05-29 22:55:07 -0600

    add colors

--- Develop/index.js
+++ Develop/index.js
@@ -83,14 +83,36 @@ function init() {
                             conversion.kill();
                           });
                     })
-
-                
-
-
-               
-
             })
     })
 }
 
 init();
+
+const colors = [ 
+    { // Green
+       wrapperBackground: "#E6E1C3",
+       headerBackground: "#C1C72C",
+       headerColor: "black",
+       photoBorderColor: "#black"
+     },
+     { // Blue
+       wrapperBackground: "#5F64D3",
+       headerBackground: "#26175A",
+       headerColor: "white",
+       photoBorderColor: "#73448C"
+     },
+     {
+      // Pink
+       wrapperBackground: "#879CDF",
+       headerBackground: "#FF8374",
+       headerColor: "white",
+       photoBorderColor: "#FEE24C"
+     },
+     { // Red
+       wrapperBackground: "#DE9967",
+       headerBackground: "#870603",
+       headerColor: "white",
+       photoBorderColor: "white"
+     }
+ ];
