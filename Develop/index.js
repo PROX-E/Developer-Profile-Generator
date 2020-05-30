1b0a7a69177c8a784fb70fcb35c05c254942b894
PROX-E <lichlord18@gmail.com>
2020-05-29 22:42:47 -0600

    update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -3,9 +3,25 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const convertFactory = require('electron-html-to');
 
-const questions = [
-  
-];
+var conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
+  });
+
+
+let data = {};
+
+let questions = [
+    {
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        message: 'What is your favorite color',
+        name: 'color',
+        type: 'list',
+        choices: ['green', 'blue', 'pink', 'red'],
+    }
+]
 
 function writeToFile(fileName, data) {
  
